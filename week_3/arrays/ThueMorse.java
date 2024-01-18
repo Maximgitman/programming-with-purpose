20cc2afb6e1a58103b2ad9287e5d100db87fcd23
Maximgitman <gorev18091993@gmail.com>
2024-01-17 20:15:59 -0800

    Refactor code to improve performance and readability

---
+++ week_3/arrays/ThueMorse.java
@@ -0,0 +1,27 @@
+public class ThueMorse {
+    public static void main(String[] args) {
+        // Read matrix size from command line
+        int n = Integer.parseInt(args[0]);
+
+        // Init sequence with Zeros
+        int[] sequence = new int[n];
+
+        // Create Thue-Morse sequence, start from 1th idx because 0th is 0
+        for (int i = 1; i < n; i++) {
+            if ((i % 2) != 0) {
+                sequence[i] = 1 - sequence[i - 1];
+            } else {
+                sequence[i] = sequence[i / 2];
+            }
+        }
+        
+        // Print pattern where ith == jth '+' and ith != jth '-'
+        for (int i = 0; i < sequence.length; i++) {
+            for (int j = 0; j < sequence.length; j++) {
+                if (sequence[i] == sequence[j]) System.out.print("+ ");
+           else if (sequence[i] != sequence[j]) System.out.print("- ");
+            }
+            System.out.println();
+        }
+    }
+}
