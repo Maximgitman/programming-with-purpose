0347714326c6cddb66402aa31a35093732b8caa7
Maximgitman <gorev18091993@gmail.com>
2024-01-18 22:12:18 -0800

    Birthday paradox init

---
+++ week_3/arrays/Birthday.java
@@ -0,0 +1,8 @@
+public class Birthday {
+    public static void main(String[] args) {
+        // Read n and triels from the command line
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        
+    }
+}
