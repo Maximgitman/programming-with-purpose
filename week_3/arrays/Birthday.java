5f8738b1568a19c6ada9af58bd3e87968f5bf9bc
Maximgitman <gorev18091993@gmail.com>
2024-01-23 16:57:13 -0800

    Refactor birthday simulation code for readability

--- week_3/arrays/Birthday.java
+++ week_3/arrays/Birthday.java
@@ -1,5 +1,6 @@
 /**
- * Simulates the birthday paradox to estimate the probability of shared birthdays in a group.
+ * Simulates the birthday paradox to estimate the probability of shared
+ * birthdays in a group.
  * 
  * @author Maksim Gorev
  */
@@ -9,29 +10,23 @@ public class Birthday {
      * Runs multiple trials of the birthday simulation for a given group size.
      * 
      * @param args Command-line arguments:
-     *            - args[0]: The number of people in the group (n).
-     *            - args[1]: The number of trials to run.
+     *             - args[0]: The number of people in the group (n).
+     *             - args[1]: The number of trials to run.
      */
     public static void main(String[] args) {
-        // Read n and trials from the command line
         int n = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
-
-        // Array to track how many trials resulted in a match for each number of people
         int[] birthdayMatches = new int[n];
 
-        // Conduct trials
         for (int i = 0; i < trials; i++) {
             boolean sameBirthday = false;
             int person = 0;
-            int[] room = new int[n];
-        
+            int[] room = new int[365]; // 365 days in a year
+
             while (!sameBirthday && person < n) {
-                // Assign a random birthday to a person
-                int birthday = (int) (Math.random() * n);
+                int birthday = (int) (Math.random() * 365);
                 room[person] = birthday;
-        
-                // Check for a match with previous birthdays
+
                 for (int p = 0; p < person; p++) {
                     if (room[p] == birthday) {
                         sameBirthday = true;
@@ -43,14 +38,13 @@ public class Birthday {
             }
         }
 
-        // Print results
         double cumulativeSum = 0.0;
         for (int b = 0; b < birthdayMatches.length; b++) {
             cumulativeSum += birthdayMatches[b];
             double fraction = cumulativeSum / trials;
             System.out.println((b + 1) + "\t" + birthdayMatches[b] + "\t" + fraction);
 
-            if (fraction > 0.5) {
+            if (fraction >= 0.5) {
                 break;
             }
         }
