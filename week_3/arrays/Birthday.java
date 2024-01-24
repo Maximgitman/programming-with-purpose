0f9901beffd0f1ce8ba2d3651fef3eb6abfbeb62
Maximgitman <gorev18091993@gmail.com>
2024-01-23 16:42:12 -0800

    Fix loop condition in Birthday.java

--- week_3/arrays/Birthday.java
+++ week_3/arrays/Birthday.java
@@ -22,21 +22,20 @@ public class Birthday {
 
         // Conduct trials
         for (int i = 0; i < trials; i++) {
-            // Simulate one trial with n people
             boolean sameBirthday = false;
             int person = 0;
             int[] room = new int[n];
-
-            while (!sameBirthday) {
+        
+            while (!sameBirthday && person < n) {
                 // Assign a random birthday to a person
-                int birthday = (int) (Math.random() * n - 1);
+                int birthday = (int) (Math.random() * n);
                 room[person] = birthday;
-
+        
                 // Check for a match with previous birthdays
                 for (int p = 0; p < person; p++) {
                     if (room[p] == birthday) {
                         sameBirthday = true;
-                        birthdayMatches[person]++; // Increment match count for this group size
+                        birthdayMatches[person]++;
                         break;
                     }
                 }
@@ -46,16 +45,12 @@ public class Birthday {
 
         // Print results
         double cumulativeSum = 0.0;
-        double experiments = trials;
         for (int b = 0; b < birthdayMatches.length; b++) {
-            // Calculate and print the fraction of trials with a match for this group size
-            double fraction = cumulativeSum / experiments;
+            cumulativeSum += birthdayMatches[b];
+            double fraction = cumulativeSum / trials;
             System.out.println((b + 1) + "\t" + birthdayMatches[b] + "\t" + fraction);
 
-            cumulativeSum += birthdayMatches[b];
-
             if (fraction > 0.5) {
-                // Stop printing once probability exceeds 0.5
                 break;
             }
         }
